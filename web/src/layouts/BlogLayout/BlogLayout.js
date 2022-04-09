a34dc9a6a787414ebafa3f2519099b3494d23546
FancyKat <fancykatbase@gmail.com>
2022-04-08 18:47:42 -0700

    Redeploy

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -2,17 +2,16 @@ import { useAuth } from '@redwoodjs/auth'
 import { Link, routes } from '@redwoodjs/router'
 
 const BlogLayout = ({ children }) => {
-
   const { isAuthenticated, currentUser, logOut } = useAuth()
 
   return (
     <>
       <header>
-      <div className="flex-between">
-        <h1>
-          <Link to={routes.home()}>Redwood Blog</Link>
-        </h1>
-        {isAuthenticated ? (
+        <div className="flex-between">
+          <h1>
+            <Link to={routes.home()}>Redwood Blog</Link>
+          </h1>
+          {isAuthenticated ? (
             <div>
               <span>Logged in as {currentUser.email}</span>{' '}
               <button type="button" onClick={logOut}>
